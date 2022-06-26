1c84691cc20071bf1bca3e571cf062bb82397497
deagustinchristian <deagustinchristian@gmail.com>
2022-06-26 15:27:24 +0000

    Forgot to save

--- foodstar/settings.py
+++ foodstar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['foodstar2022.herokuapp.com']
+ALLOWED_HOSTS = ['foodstar2022.herokuapp.com', 'localhost']
 
 
 # Application definition
