92ccb2d6184629e855233e4a74c7d846021de0db
deagustinchristian <deagustinchristian@gmail.com>
2022-06-28 12:08:23 +0000

    Add class and build models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipe_posts")
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    prep_time = models.DurationField()
+    cook_time = models.DurationField()
+    servings = models.IntegerField()
+    likes = models.ManyToManyField(User, related_name='recipe_likes', blank=True)
+    
