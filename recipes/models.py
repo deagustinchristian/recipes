23cfc5b21360bcdd33953239a006a77e4b805d9c
deagustinchristian <deagustinchristian@gmail.com>
2022-06-28 13:13:55 +0000

    Add comment model

--- recipes/models.py
+++ recipes/models.py
@@ -2,12 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+
 STATUS = ((0, "Draft"), (1, "Published"))
 
-class Recipe(models.Model):
+
+class Post(models.Model):
     title = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipe_posts")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recipe_posts"
+    )
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
@@ -16,20 +20,22 @@ class Recipe(models.Model):
     prep_time = models.DurationField()
     cook_time = models.DurationField()
     servings = models.IntegerField()
-    likes = models.ManyToManyField(User, related_name='recipe_likes', blank=True)
+    likes = models.ManyToManyField(
+        User, related_name='recipepost_like', blank=True)
 
     class Meta:
-        ordering = ['-created_on']
+        ordering = ["-created_on"]
 
     def __str__(self):
-        return.title
-    
+        return self.title
+
     def number_of_likes(self):
         return self.likes.count()
 
-class Comment(models.Model):
 
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
     name = models.CharField(max_length=70)
     email = models.EmailField()
     body = models.TextField()
@@ -37,7 +43,7 @@ class Comment(models.Model):
     approved = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ['created_on']
+        ordering = ["created_on"]
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
