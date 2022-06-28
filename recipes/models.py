c73e35d3e5670ddb8bae77d4b4627afcca24b623
deagustinchristian <deagustinchristian@gmail.com>
2022-06-28 12:20:30 +0000

    Add comment class to models

--- recipes/models.py
+++ recipes/models.py
@@ -17,4 +17,27 @@ class Recipe(models.Model):
     cook_time = models.DurationField()
     servings = models.IntegerField()
     likes = models.ManyToManyField(User, related_name='recipe_likes', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return.title
     
+    def number_of_likes(self):
+        return self.likes.count()
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=70)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
