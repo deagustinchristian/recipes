eea6577ad3c5f73b5ed87d67b175d8d66b2d189e
deagustinchristian <deagustinchristian@gmail.com>
2022-07-01 13:26:17 +0000

    Add a class for likes

--- recipes/views.py
+++ recipes/views.py
@@ -63,3 +63,13 @@ class PostDetail(View):
                 "liked": liked
             },
         )
+
+class PostLike(View):
+
+    def post(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
+
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
