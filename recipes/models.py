ddabaa01109deb5a7f627c62532b1f9a2e114a4f
deagustinchristian <deagustinchristian@gmail.com>
2022-07-06 09:23:09 +0000

    Check code in Pep8, fix errors

--- recipes/models.py
+++ recipes/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from django.urls import reverse
-from django.utils.text import slugify 
+from django.utils.text import slugify
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
